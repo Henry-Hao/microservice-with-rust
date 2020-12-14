7ec3b7abcb19b2e2654f47e924016d4deb5d5d5e
Henry Hao <haoyixian1994@hotmail.com>
2020-12-13 21:31:51 -0500

    1. Added TOML config file. 2. Read address parameter in the order :     - Config file     - Environment variable     - Command line     - Default

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use std::net::SocketAddr;
 use hyper::{Body, Response, Server };
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use log::{debug, info, trace};
-use clap::*;
+use log::{debug, info, trace, warn};
+use clap::{ crate_name, crate_authors, crate_version, crate_description, Arg, App, AppSettings, SubCommand };
+use serde_derive::Deserialize;
+use std::fs::File;
+use std::io::{Read, self};
+
+#[derive(Deserialize)]
+struct Config {
+    address: SocketAddr
+}
 
 fn main() {
     let matches = App::new(crate_name!())
@@ -35,16 +43,39 @@ fn main() {
     info!("Rand microservice - v0.1.0");
     trace!("Starting... ");
     if let Some(sub_m) = matches.subcommand_matches("run") {
+        let config = File::open("microservice.toml")
+            .and_then(|mut file| {
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer)?;
+                Ok(buffer)
+            }).and_then(|buffer|{
+                toml::from_str::<Config>(&buffer)
+                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+            }).map_err(|err|{
+                warn!("Can't read config file :{}", err);
+            })
+        .ok();
         if let Some(_) = sub_m.subcommand_matches("key") {
             let random_byte = rand::random::<u8>();
             println!("Your secret key is:{}", random_byte);
         } else {
-            let addr: SocketAddr = sub_m.value_of("address")
-                .map(|x| x.to_owned())
-                .or(env::var("ADDRESS").ok())
-                .unwrap_or("127.0.0.1:8080".into())
-                .parse()
-                .expect("Cannot parse the address");
+            let addr: SocketAddr = config.map(|c| {
+                debug!("Trying config file");
+                c.address
+            })
+            .or_else(|| {
+                debug!("Trying env variable");
+                env::var("ADDRESS").ok().map(|addr| addr.parse().unwrap())
+            })
+            .or_else(|| {
+                debug!("Trying command line");
+                sub_m.value_of("address").map(|addr| addr.to_owned().parse().unwrap())
+            })
+            .or_else(|| {
+                debug!("Using default");
+                Some(([127, 0, 0, 1], 9999).into())
+            })
+            .expect("Cannot parse the address");
             debug!("Trying to bind server with address {}:", addr);
             let builder = Server::bind(&addr);
             trace!("Creating service handler...");
