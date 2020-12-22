d3084f9eaf0e523885d11b0b9234fc88a3c47e06
Henry Hao <haoyixian1994@hotmail.com>
2020-12-21 21:56:48 -0500

    Implement an image server

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,103 @@
-use serde::{Serialize, Deserialize};
-use std::net::SocketAddr;
-use hyper::{ Body, Response, Request, Server, Error, StatusCode, Method };
-use futures::{ future, Future, Stream };
+use std::path::Path;
+use std::io;
+use tokio::fs;
+use tokio::prelude::Future;
+use hyper::{ Request, Method, Body, Server, Response, StatusCode };
 use hyper::service::service_fn;
-use failure::{format_err, self};
-use std::ops::Range;
-use rand::Rng;
-use rand::distributions::{Uniform, Normal, Bernoulli};
-use base64::STANDARD;
-use base64_serde::base64_serde_type;
-use queryst;
+use futures::future;
+use hyper::rt::{ Stream};
+use rand::{ thread_rng, Rng, distributions::Alphanumeric };
+use lazy_static::lazy_static;
+use regex::Regex;
+use hyper_staticfile::FileChunkStream;
 
-mod color;
-use color::Color;
+const INDEX: &'static str= r#"
+<!doctype html>
+<html>
+    <head>
+        <title> Rust Microservice </title>
+    </head>
+    <body>
+        <h3> Image service </h3>
+    </body>
+</html>
+"#;
 
-static INDEX:&'static str = "Microservice Rust";
-base64_serde_type!(Base64Standard, STANDARD);
-
-#[derive(Serialize)]
-#[serde(rename_all="lowercase")]
-enum RngResponse {
-    Value(f64),
-    #[serde(with="Base64Standard")]
-    Bytes(Vec<u8>),
-    Color(Color)
-
+lazy_static!{
+    static ref DOWNLOAD_FILE: Regex = Regex::new("^/download/(?P<filename>\\w{20})?$").unwrap();
 }
 
-
-#[derive(Deserialize, Serialize)]
-#[serde(tag = "distribution", rename_all = "lowercase", content = "parameters")]
-enum RngRequest {
-    Uniform {
-        #[serde(flatten)]
-        range: Range<i32>
-    },
-    Normal {
-        mean: f64,
-        std_dev: f64
-    },
-    Bernoulli {
-        p: f64
-    },
-    Shuffle {
-        #[serde(with="Base64Standard")]
-        data: Vec<u8>
-    },
-    Color {
-        from: Color,
-        to: Color
-    }
-}
 
 fn main() {
-    let addr:SocketAddr = ([127, 0, 0, 1], 8080).into();
+    let files = Path::new("./files");
+    std::fs::create_dir(files).ok();
+    let addr = ([127, 0, 0, 1], 8080).into();
     let builder = Server::bind(&addr);
-    let server = builder.serve(|| {
-        service_fn(|req| microservice_handler(req))
+    let server = builder.serve(move || {
+        service_fn(move |req| microservice_handler(req, &files))
     });
     let server = server.map_err(drop);
     hyper::rt::run(server);
+
 }
 
-fn microservice_handler(req:Request<Body>) -> Box<dyn Future<Item=Response<Body>, Error=Error> + Send>{
-    match(req.method(), req.uri().path()) {
+
+fn microservice_handler(req: Request<Body>, files: &Path) -> Box<dyn Future<Item=Response<Body>, Error=std::io::Error> + Send> {
+    let method = req.method();
+    let path = req.uri().path();
+    match (method, path) {
         (&Method::GET, "/") => {
             Box::new(future::ok(Response::new(INDEX.into())))
         },
-        (&Method::POST, "/random") => {
-            let format = {
-                let uri = req.uri().query().unwrap_or("");
-                let query = queryst::parse(uri).unwrap_or(serde_json::Value::Null);
-                query["format"].as_str().unwrap_or("json").to_string()
-            };
-            let body = req.into_body().concat2()
-                .map(move |chunk|{
-                    let res = serde_json::from_slice::<RngRequest>(chunk.as_ref())
-                        .map(body_handler)
-                        .map_err(From::from)
-                        .and_then(|resp| serialize(&format, &resp));
-                    match res {
-                        Ok(body) => {
-                            Response::new(body.into())
-                        }, 
-                        Err(err) => {
-                            Response::builder()
-                                .status(StatusCode::UNPROCESSABLE_ENTITY)
-                                .body(err.to_string().into())
-                                .unwrap()
-                        }
-                    }
-                });
+        (&Method::POST, "/upload") => {
+            let name: String = thread_rng().sample_iter(&Alphanumeric).take(20).collect();
+            let mut filepath = files.to_path_buf();
+            filepath.push(&name);
+            let create_file = fs::File::create(filepath);
+            let write = create_file.and_then(|file| {
+                req.into_body()
+                    .map_err(other)
+                    .fold(file, |file, chunk| {
+                        tokio::io::write_all(file, chunk)
+                            .map(|(file,_)| file)
+                    })
+            });
+            let body = write.map(|_| {
+                Response::new(name.into())
+            });
             Box::new(body)
         },
+        (&Method::GET, path) if path.starts_with("/download") => {
+            if let Some(cap) = DOWNLOAD_FILE.captures(path) {
+                let name = cap.name("filename").unwrap().as_str();
+                let mut filepath = files.to_path_buf();
+                filepath.push(&name);
+                let open_file = fs::File::open(filepath);
+                let body = open_file.map(|file| {
+                    let stream =FileChunkStream::new(file);
+                    Response::new(Body::wrap_stream(stream))
+                });
+                Box::new(body)
+
+            } else {
+                response_with_code(StatusCode::NOT_FOUND)
+            }
+        },
         _ => {
-            Box::new(future::ok(Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty()).unwrap()))
+            response_with_code(StatusCode::METHOD_NOT_ALLOWED)
         }
     }
+
 }
 
 
-fn body_handler(request: RngRequest) -> RngResponse {
-    let mut rng = rand::thread_rng();
-    match request {
-        RngRequest::Uniform { range } => {
-            RngResponse::Value(rng.sample(Uniform::from(range)) as f64)
-        },
-        RngRequest::Normal { mean, std_dev } => {
-            RngResponse::Value(rng.sample(Normal::new(mean, std_dev)) as f64)
-        },
-        RngRequest::Bernoulli { p } => {
-            RngResponse::Value(rng.sample(Bernoulli::new(p)) as i8 as f64)
-        },
-        RngRequest::Shuffle { mut data } => {
-            rng.shuffle(&mut data);
-            RngResponse::Bytes(data)
-        },
-        RngRequest::Color { from, to } => {
-            let red = rng.sample(Uniform::new_inclusive(from.red, to.red));
-            let green = rng.sample(Uniform::new_inclusive(from.green, to.green));
-            let blue = rng.sample(Uniform::new_inclusive(from.blue, to.blue));
-            RngResponse::Color(Color{red, green, blue})
-        }
-    }
+fn other<E>(err: E) -> io::Error 
+where E: Into<Box<dyn std::error::Error + Send + Sync>>
+{
+    io::Error::new(io::ErrorKind::Other, err)
 }
 
-fn serialize(format: &str, resp: &RngResponse) -> Result<Vec<u8>, failure::Error> {
-    println!("{:?}",serde_json::to_string(resp));
-    match format {
-        "json" => Ok(serde_json::to_vec(resp)?),
-        "cbor" => Ok(serde_cbor::to_vec(resp)?),
-        _ => Err(format_err!("unsupported format:{}", format))
-    }
-}
 
+fn response_with_code(code: StatusCode) -> Box<dyn Future<Item=Response<Body>, Error=std::io::Error> + Send> {
+    Box::new(future::ok(Response::builder().status(code).body(Body::empty()).unwrap()))
+}
