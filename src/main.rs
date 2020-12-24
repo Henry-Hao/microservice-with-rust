a8fb44f79a9890226c2ba5beea6a07a517920321
Henry Hao <haoyixian1994@hotmail.com>
2020-12-24 18:58:35 -0500

    Use r2d2::Pool to get a connection Added "import" subcommand to read csv from stdin

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use clap::{
     App, AppSettings, Arg, SubCommand
 };
 use postgres:: {
-    Error as DbError, NoTls, Client, Config
+    Error as DbError, NoTls, Client, Config, config::SslMode
 };
+use rayon::prelude::*;
+use serde_derive::Deserialize;
+use r2d2_postgres::PostgresConnectionManager;
+use r2d2::Pool;
 
 #[derive(Debug)]
 enum Error {
     CliError(std::num::ParseIntError),
-    DbError(DbError)
+    DbError(DbError),
+    CsvError(csv::Error),
+    PoolError(r2d2::Error)
 }
 
 impl std::fmt::Display for Error {
@@ -21,6 +27,12 @@ impl std::fmt::Display for Error {
             },
             Error::DbError(err) => {
                 write!(f, "DbError:{}", err)
+            },
+            Error::CsvError(err) => {
+                write!(f, "CsvError:{}", err)
+            },
+            Error::PoolError(err) => {
+                write!(f, "PoolError:{}", err)
             }
         }
     }
@@ -37,12 +49,28 @@ impl From<DbError> for Error {
         Error::DbError(err)
     }
 }
+impl From<csv::Error> for Error {
+    fn from(err:csv::Error) -> Self {
+        Error::CsvError(err)
+    }
+}
+impl From<r2d2::Error> for Error {
+    fn from(err:r2d2::Error) -> Self {
+        Error::PoolError(err)
+    }
+}
 
+#[derive(Deserialize)]
+struct User {
+    name: String,
+    email: String
+}
 
 
 const CMD_CREATE: &str = "create";
 const CMD_ADD: &str = "add";
 const CMD_LIST: &str = "list";
+const CMD_IMPORT: &str= "import";
 
 
 fn main() -> Result<(), Error> {
@@ -80,29 +108,43 @@ fn main() -> Result<(), Error> {
                          .required(true)
                          .index(2)))
         .subcommand(SubCommand::with_name(CMD_LIST).about("print list of users"))
+        .subcommand(SubCommand::with_name(CMD_IMPORT).about("import users from csv"))
         .get_matches();
     let host = matches.value_of("host").unwrap_or("localhost");
-    let port = matches.value_of("port").unwrap_or("5432");
+    let port = matches.value_of("port").unwrap_or("5432").parse::<u16>()?;
     let database = matches.value_of("database").unwrap_or("postgres");
-    let mut conn = Config::new()
-        .host(host)
-        .port(port.parse::<u16>()?)
-        .user(database)
-        .connect(NoTls)?;
+    let mut config = Config::new();
+    config.host(host)
+        .port(port)
+        .user(database);
+    let manager = PostgresConnectionManager::new(config.to_owned(), NoTls);
+    let pool = r2d2::Pool::new(manager).unwrap();
     match matches.subcommand() {
         (CMD_CREATE, _) => {
-            create_table(&mut conn)?;
+            create_table(pool)?;
         },
         (CMD_LIST, _) => {
-            let users = list_users(&mut conn)?;
-            for (name, email) in users {
-                println!("Name: {:20} Email: {:20}", name, email);
+            let users = list_users(pool)?;
+            for user in users {
+                println!("Name: {:20} Email: {:20}", user.name, user.email);
             }
         },
         (CMD_ADD, Some(matches)) => {
             let name = matches.value_of("name").unwrap();
             let email = matches.value_of("email").unwrap();
-            create_user(&mut conn, name, email)?;
+            create_user(pool, &User{ name: name.to_string(), email: email.to_string() })?;
+        },
+        (CMD_IMPORT, _) => {
+            let mut rdr = csv::Reader::from_reader(std::io::stdin());
+            let mut users = Vec::new();
+            for user in rdr.deserialize() {
+                users.push(user?)
+            }
+            users.par_iter().map(|user| -> Result<(), failure::Error>{
+                let pool = pool.clone();
+                create_user(pool, &user)?;
+                Ok(())
+            }).for_each(drop);
         },
         _ => {
             matches.usage();
@@ -111,7 +153,8 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn create_table(conn: &mut Client) -> Result<(), Error> {
+fn create_table(pool: r2d2::Pool<PostgresConnectionManager<NoTls>>) -> Result<(), Error> {
+    let mut conn = pool.get()?;
     conn.execute("CREATE TABLE users (
                         id SERIAL PRIMARY KEY,
                         name VARCHAR NOT NULL,
@@ -121,15 +164,17 @@ fn create_table(conn: &mut Client) -> Result<(), Error> {
 }
 
 
-fn create_user(conn: &mut Client, name: &str, email: &str) -> Result<(), Error> {
-    conn.execute("INSERT INTO users (name, email) VALUES ($1, $2)", &[&name, &email])
+fn create_user(pool: r2d2::Pool<PostgresConnectionManager<NoTls>>, user: &User) -> Result<(), Error> {
+    let mut conn = pool.get()?;
+    conn.execute("INSERT INTO users (name, email) VALUES ($1, $2)", &[&user.name, &user.email])
         .map(drop)
         .map_err(From::from)
 }
 
-fn list_users(conn: &mut Client) -> Result<Vec<(String, String)>, Error> {
+fn list_users(pool: r2d2::Pool<PostgresConnectionManager<NoTls>>) -> Result<Vec<User>, Error> {
+    let mut conn = pool.get()?;
     let res = conn.query("SELECT name, email FROM users", &[])?.into_iter()
-        .map(|row|(row.get(0), row.get(1)))
+        .map(|row| User {name: row.get(0), email: row.get(1)})
         .collect();
     Ok(res)
 }
