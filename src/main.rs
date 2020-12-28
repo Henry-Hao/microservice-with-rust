6b8f045e5f229facaa33b99ede717b4195b1e4a6
Henry Hao <haoyixian1994@hotmail.com>
2020-12-28 00:35:46 -0500

    Supported DynamoDb in AWS

--- src/main.rs
+++ src/main.rs
@@ -3,67 +3,111 @@ use clap::{
     crate_name, crate_authors, crate_version, crate_description,
     App, AppSettings, Arg, SubCommand
 };
-use chrono::offset::Utc;
-use r2d2_mongodb::{ ConnectionOptions, MongodbConnectionManager };
-use mongodb::db::ThreadedDatabase;
-use bson::{ doc, bson };
-use serde_derive::Deserialize;
-
-#[derive(Debug, Deserialize)]
-struct Activity {
-    user_id: String,
-    activity: String,
-    datetime: String
-}
+use chrono::Utc;
+use failure::{Error, format_err};
+use rusoto_core::Region;
+use rusoto_dynamodb::{
+    AttributeValue, DynamoDb, DynamoDbClient, QueryInput, UpdateItemInput
+};
+use std::collections::HashMap;
 
 #[derive(Debug)]
-enum Error {
-    CliError(std::num::ParseIntError),
-    DbError(mongodb::error::Error),
-    PoolError(r2d2::Error),
-    DecoderError(mongodb::DecoderError)
+struct Location {
+    user_id: String,
+    timestamp: String,
+    longitude: String,
+    latitude: String
 }
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::CliError(err) => {
-                write!(f, "CliError:{}", err)
-            },
-            Error::DbError(err) => {
-                write!(f, "DbError:{}", err)
-            },
-            Error::DecoderError(err) => {
-                write!(f, "DecoderError:{}", err)
-            },
-            Error::PoolError(err) => {
-                write!(f, "PoolError:{}", err)
-            }
-        }
+impl Location {
+    fn from_map(map: HashMap<String, AttributeValue>) -> Result<Location, Error> {
+        let user_id = map
+            .get("Uid")
+            .ok_or_else(|| format_err!("No Uid in record"))
+            .and_then(attr_to_string)?;
+        let timestamp = map
+            .get("TimeStamp")
+            .ok_or_else(|| format_err!("No TimeStamp in record"))
+            .and_then(attr_to_string)?;
+        let latitude = map
+            .get("Latitude")
+            .ok_or_else(|| format_err!("No Latitude in record"))
+            .and_then(attr_to_string)?;
+        let longitude = map
+            .get("Longitude")
+            .ok_or_else(|| format_err!("No Longitude in record"))
+            .and_then(attr_to_string)?;
+        let location = Location {
+            user_id,
+            timestamp,
+            latitude,
+            longitude
+        };
+        Ok(location)
     }
 }
 
-impl std::error::Error for Error { }
-impl From<std::num::ParseIntError> for Error {
-    fn from(err: std::num::ParseIntError) -> Self {
-        Error::CliError(err)
+fn add_location(conn: &DynamoDbClient, location: Location) -> Result<(), Error> {
+    let mut key: HashMap<String, AttributeValue> = HashMap::new();
+    key.insert("Uid".into(), s_attr(location.user_id));
+    key.insert("TimeStamp".into(), s_attr(location.timestamp));
+    let expression = format!("SET Latitude = :y, Longitude = :x");
+    let mut values = HashMap::new();
+    values.insert(":y".into(), s_attr(location.latitude));
+    values.insert(":x".into(), s_attr(location.longitude));
+    let update = UpdateItemInput {
+        table_name: "Locations".into(),
+        key,
+        update_expression: Some(expression),
+        expression_attribute_values: Some(values),
+        ..Default::default()
+    };
+    conn.update_item(update)
+        .sync()
+        .map(drop)
+        .map_err(Error::from)
+
+}
+
+fn list_locations(conn: &DynamoDbClient, user_id: String) -> Result<Vec<Location>, Error> {
+    let expression = format!("Uid = :uid");
+    let mut values = HashMap::new();
+    values.insert(":uid".into(), s_attr(user_id));
+    let query = QueryInput {
+        table_name: "Locations".into(),
+        key_condition_expression: Some(expression),
+        expression_attribute_values: Some(values),
+        ..Default::default()
+    };
+    let items = conn.query(query)
+        .sync()?
+        .items
+        .ok_or_else(|| format_err!("No items"))?;
+
+    let mut locations = Vec::new();
+    for item in items {
+        let location = Location::from_map(item)?;
+        locations.push(location);
+    }
+
+    Ok(locations)
+}
+
+fn s_attr(s: String) -> AttributeValue {
+    AttributeValue {
+        s: Some(s),
+        ..Default::default()
     }
 }
-impl From<mongodb::error::Error> for Error {
-    fn from(err:mongodb::error::Error) -> Self {
-        Error::DbError(err)
+
+fn attr_to_string(attr: &AttributeValue) -> Result<String, Error> {
+    if let Some(value) = &attr.s {
+        Ok(value.to_owned())
+    } else {
+        Err(format_err!("no string value"))
     }
 }
-impl From<r2d2::Error> for Error {
-    fn from(err:r2d2::Error) -> Self {
-        Error::PoolError(err)
-    }
-}
-impl From<mongodb::DecoderError> for Error {
-    fn from(err:mongodb::DecoderError) -> Self {
-        Error::DecoderError(err)
-    }
-}
+
 
 const CMD_ADD: &str = "add";
 const CMD_LIST: &str = "list";
@@ -75,89 +119,75 @@ fn main() -> Result<(), Error> {
         .version(crate_version!())
         .about(crate_description!())
         .setting(AppSettings::SubcommandRequired)
-        .arg(Arg::with_name("host")
-             .short("h")
-             .long("host")
-             .value_name("HOST")
-             .help("Set the host of db connection")
+        .arg(Arg::with_name("region")
+             .short("r")
+             .long("region")
+             .value_name("REGION")
+             .help("Set the region of the AWS account")
              .takes_value(true))
-        .arg(Arg::with_name("port")
-             .short("p")
-             .long("port")
-             .value_name("PORT")
-             .help("Set the port of db connection")
+        .arg(Arg::with_name("endpoint")
+             .short("e")
+             .long("endpoint")
+             .value_name("URL")
+             .help("Set the endpoint of the AWS account")
              .takes_value(true))
-        .arg(Arg::with_name("database")
-             .short("d")
-             .long("db")
-             .value_name("DATABASE")
-             .help("Set the databse of db connection")
-             .takes_value(true))
-        .subcommand(SubCommand::with_name(CMD_ADD).about("create a session record")
+        .subcommand(SubCommand::with_name(CMD_ADD).about("create a location record")
                     .arg(Arg::with_name("USER_ID")
                          .help("Set the userid")
                          .required(true)
                          .index(1))
-                    .arg(Arg::with_name("ACTIVITY")
-                         .help("Set the activity")
+                    .arg(Arg::with_name("LATITUDE")
+                         .help("Set the latitude")
                          .required(true)
-                         .index(2)))
-        .subcommand(SubCommand::with_name(CMD_LIST).about("print list of activities"))
+                         .index(2))
+                    .arg(Arg::with_name("LONGITUDE")
+                         .help("Set the longitude")
+                         .required(true)
+                         .index(3)))
+        .subcommand(SubCommand::with_name(CMD_LIST).about("print list of locations")
+                    .arg(Arg::with_name("USER_ID")
+                         .help("user id to filter records")
+                         .required(true)
+                         .index(1)))
         .get_matches();
-    let host = matches.value_of("host").or(Some("127.0.0.1")).unwrap();
-    let port = matches.value_of("port").unwrap_or("27017").parse::<u16>()?;
-    let database = matches.value_of("database").unwrap_or("admin");
-    let manager = MongodbConnectionManager::new(
-        ConnectionOptions::builder()
-        .with_host(host, port)
-        .with_db(database)
-        .build()
-        );
-    let pool = r2d2::Pool::builder().max_size(4).build(manager)?;
+    let region = matches.value_of("endpoint").map(|endpoint| {
+        Region::Custom {
+            name: "custom".into(),
+            endpoint: endpoint.into()
+        }
+    })
+    .ok_or_else(||format_err!("Region not set"))
+    .or_else(|_| {
+        matches.value_of("region")
+            .unwrap_or("us-east-1")
+            .parse()
+    })?;
+    let client = DynamoDbClient::new(region);
     match matches.subcommand() {
-        (CMD_LIST, _) => {
-            let activities = list_activity(pool)?;
-            for activity in activities {
-                println!("User:{:20}, Activity: {:20}, Datetime: {:20}",
-                         activity.user_id, activity.activity, activity.datetime);
+        (CMD_LIST, Some(matches)) => {
+            let user_id = matches.value_of("USER_ID").unwrap().to_string();
+            for location in list_locations(&client, user_id)? {
+                println!("{:?}", location);
             }
-
         },
         (CMD_ADD, Some(matches)) => {
             let user_id = matches.value_of("USER_ID").unwrap().to_string();
-            let activity = matches.value_of("ACTIVITY").unwrap().to_string();
-            let activity = Activity {
+            let timestamp = Utc::now().to_string();
+            let latitude = matches.value_of("LATITUDE").unwrap().to_string();
+            let longitude = matches.value_of("LONGITUDE").unwrap().to_string();
+            let location = Location {
                 user_id,
-                activity,
-                datetime: Utc::now().to_string()
+                timestamp,
+                latitude,
+                longitude
             };
-            add_activity(pool, activity);
+            add_location(&client, location)?
+
         },
         _ => {
             matches.usage();
         }
     }
     Ok(())
-}
-
-fn add_activity(pool: r2d2::Pool<MongodbConnectionManager>, activity: Activity) -> Result<(), Error> {
-    let doc = doc! {
-        "user_id": activity.user_id,
-        "activity": activity.activity,
-        "datetime": activity.datetime
-    };
-    let coll = pool.get()?.collection("activities");
-    coll.insert_one(doc, None).map(drop).map_err(From::from)
-}
-
-fn list_activity(pool: r2d2::Pool<MongodbConnectionManager>) -> Result<Vec<Activity>, Error> {
-    let coll = pool.get()?.collection("activities");
-    coll.find(None,None)?
-        .try_fold(Vec::new(), |mut vec, doc| {
-            let doc = doc?;
-            let activity: Activity = bson::from_bson(bson::Bson::Document(doc))?;
-            vec.push(activity);
-            Ok(vec)
-        })
 }
 
